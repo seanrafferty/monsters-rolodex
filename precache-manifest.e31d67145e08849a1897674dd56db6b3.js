52d184f8309a5b9817f7406de2bec57e7f0a0adf
Sean Rafferty <seanraffertyregisters@gmail.com>
2019-09-09 18:56:25 -0500

    Updates

---
+++ precache-manifest.e31d67145e08849a1897674dd56db6b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfba2cc7c6e4f5a5ce7ef112a969084",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce4588e090dd03103343",
+    "url": "/monsters-rolodex/static/css/main.683a37ce.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "ce4588e090dd03103343",
+    "url": "/monsters-rolodex/static/js/main.435a5521.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
